873986e07e11e3261fcfd26234b75e41f6a10f69
Amarildo Lucas <vmarildo@gmail.com>
2019-01-23 10:46:41 -0200

    return signature string

--- lib/kamba_signature_generation/api.rb
+++ lib/kamba_signature_generation/api.rb
@@ -3,12 +3,11 @@ module KambaSignatureGeneration
     def self.generate_signature(checkout)
       time = Time.now.utc.httpdate
       secret_key = KambaSignatureGeneration.secret_key
-
       json_string = checkout.to_json
       canonical_string = "POST,application/json,#{generate_md5(json_string)},/v1/checkouts,#{time}"
       digest = OpenSSL::Digest.new('sha1')
       hmac_signature = OpenSSL::HMAC.hexdigest(digest, secret_key, canonical_string)
-      time + '.' + Base64.strict_encode64(hmac_signature)
+      "#{time}.#{Base64.strict_encode64(hmac_signature)}"
     end
 
     def self.generate_md5(checkout)
