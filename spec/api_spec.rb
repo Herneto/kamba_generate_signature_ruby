eaa3f284d6adc496fc59a39ec38dc0981cd68665
Amarildo Lucas <vmarildo@gmail.com>
2019-01-23 10:57:19 -0200

    update tests

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -4,17 +4,17 @@ describe 'KambaSignatureGeneration::API' do
   describe 'Generating Signature' do
     before(:each) do
       @checkout = {
-        'channel'=>'WEB',
-        'initial_amount'=>5500,
-        'notes'=> 'Alguma note exemplo.',
-        'redirect_url_success'=> 'http://amarildolucas.com/curso/aplicativo-movel-com-swift/sucesso'
+        channel: 'WEB',
+        initial_amount: 5500,
+        notes: 'Alguma note exemplo.',
+        redirect_url_success: 'http://amarildolucas.com/curso/aplicativo-movel-com-swift/sucesso'
       }
-      KambaSignatureGeneration.secret_key = '123'
+      KambaSignatureGeneration.secret_key = 'secret_key'
     end
 
-    it 'should generate signature' do
+    it 'should generate signature string' do
       signature = KambaSignatureGeneration::API.generate_signature(@checkout)
-      signature.should_not == '123'
+      expect(sinature).to_not be_nil
     end
   end
 end
